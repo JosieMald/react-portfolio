6da14147bdbe41f66fb897e3d21164f2cdf3e4bb
Lucre <josielindamaldonado@gmail.com>
2021-05-19 23:28:04 -0400

    removed work class and fixed css.

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -40,7 +40,7 @@ function Work() {
             src="https://marketingland.com/wp-content/ml-loads/2015/02/ecommerce-shopping-cart-keyboard-ss-1920.jpg"
             className="projects"
           />
-          <div className="work-gallery-sub container">
+          <div className="work-gallery container">
             <h5 className="mt-2">E Commerce Back End</h5>
             <p>App to view e commerce DB</p>
           </div>
@@ -53,7 +53,7 @@ function Work() {
             src="https://thumbs.dreamstime.com/b/planning-day-day-blank-day-planner-pen-your-blue-96797256.jpg"
             className="projects"
           />
-          <div className="work-gallery-sub container">
+          <div className="work-gallery container">
             <h5 className="mt-2">Event Planner</h5>
             <p>Calendar to keep you organized</p>
           </div>
@@ -63,7 +63,7 @@ function Work() {
           href="https://github.com/JosieMald/professional-readME-generator"
         >
           <img src={readme} alt="Readme Project" className="projects" />
-          <div className="work-gallery-sub container">
+          <div className="work-gallery container">
             <h5 className="mt-2">ReadME Generator</h5>
             <p>Create a professional readME</p>
           </div>
@@ -73,7 +73,7 @@ function Work() {
           href="https://github.com/JosieMald/password-generator"
         >
           <img src={passwordGenerator} alt="Password Generator Project" className="projects" />
-          <div className="work-gallery-sub container">
+          <div className="work-gallery container">
             <h5 className="mt-2">PW Generator</h5>
             <p>Generate a random password</p>
           </div>
