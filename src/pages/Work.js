8db85fa6c104f34dc9e2c4d848d15217210da204
Lucre <josielindamaldonado@gmail.com>
2022-05-30 18:26:56 -0400

    changed all other card code to graduate and change h3 and links.

--- src/pages/Work.js
+++ src/pages/Work.js
@@ -2,7 +2,7 @@ import React from "react";
 import snoopie from "../images/snoopie.jpg";
 import graduate from "../images/graduate.jpg";
 import readme from "../images/readme.jpg";
-import passwordGenerator from "../images/password-generator.jpg";
+import projectTracker from "../images/project-tracker.jpg";
 import enlighten from "../images/enlighten-me.jpg";
 import searchEngine from "../images/search-engine.jpg";
 import eCommerce from "../images/e-commerce.jpg";
@@ -16,17 +16,41 @@ function Work() {
   return (
     <div className="container">
       <div className="container mt-3 col-12 text-center">
-        <h3 id="work">Work</h3>
+        <h3 id="work">Projects</h3>
       </div>
       <div className="col-12 container d-flex flex-wrap justify-content-center" >
         <figure className=" mt-5 p-5 card col-12 col-md-6" style={{ width: "36rem" }}>
           <div className="snip1273">
           <img
             src={graduate}
-            alt="sample72"
+            alt="graduates"
             />
             <figcaption>
               <h3 className="text-center">University 4 me</h3>
+              <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+              </ul>
+              <div>
+              <a
+                href="https://agile-thicket-84742.herokuapp.com/"
+                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn" target="blank"
+              >
+                {"\u2192"}
+              </a>
+              </div>
+            </figcaption>
+          </div>
+        </figure>
+        <figure className=" mt-5 p-5 card col-12 col-md-6" style={{ width: "36rem" }}>
+          <div className="snip1273">
+          <img
+            src={searchEngine}
+            alt="computer keyboard"
+            />
+            <figcaption>
+              <h3 className="text-center">LOC Search Engine</h3>
               <ul>
                 <li>hello</li>
                 <li>hello</li>
@@ -43,102 +67,156 @@ function Work() {
             </figcaption>
           </div>
         </figure>
-        {/* <div
-          className="mt-5 p-5 card col-12 col-md-6"
-          style={{ width: "36rem" }}
-        >
-          <div class="">
-            <img src={graduate} className="main-img border border-2 border-warning" alt="graduates" />
-            <div class="card-body border border-warning border-top-0 border-2 rounded-bottom d-flex">
-              <p class="card-text col-10">Customize College Searches</p>
-              <a
-                href="https://josiemald.github.io/library-congress-SE/"
-                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn"
-              >
-                {"\u2192"}
-              </a>
-            </div>
-          </div>
-        </div> */}
-        <div
-          className="mt-5 p-5 card col-12 col-md-6"
-          style={{ width: "36rem" }}
-        >
-          <div class="">
-            <img
-              src={searchEngine}
-              className="main-img border border-2 border-warning"
-              alt="graduates"
+        <figure className=" mt-5 p-5 card col-6 col-md-4 sub-card">
+          <div className="snip1273">
+          <img
+            src={snoopie}
+            alt="Dog"
+            className="projects"
             />
-            <div class="card-body border border-warning border-top-0 border-2 rounded-bottom d-flex">
-              <p class="card-text col-10">Library of Congress Search Engine</p>
+            <figcaption>
+              <h3 className="text-center">Snoopie</h3>
+              <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+              </ul>
+              <div>
               <a
-                href="https://josiemald.github.io/library-congress-SE/"
-                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn"
+                href="https://sttorek.github.io/snoopie/"
+                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn" target="blank"
               >
                 {"\u2192"}
               </a>
-            </div>
+              </div>
+            </figcaption>
           </div>
-        </div>
-        <div className="mt-5 p-5 border border-warning card col-6 col-md-4 sub-card">
-          <div class="">
-            <img src={snoopie} className="projects" alt="graduates" />
-            <div class="card-body border border-warning border-top-0 border-2 rounded-bottom d-flex">
-              <p class="card-text col-10">Find the perfect pet</p>
-              <a
-                href="https://josiemald.github.io/library-congress-SE/"
-                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn"
-              >
-                {"\u2192"}
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 p-5 border border-warning card col-6 col-md-4 sub-card">
-          <div class="">
-            <img src={enlighten} className="projects" alt="graduates" />
-            <div class="card-body">
-              <p class="card-text">Lifestyle change blog</p>
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 p-5 border border-warning card col-6 col-md-4 sub-card">
-          <div class="">
-            <img src={eCommerce} className="projects" alt="keyboard keys" />
-            <div class="card-body">
-              <p class="card-text">E Commerce Database</p>
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 p-5 border border-warning card col-6 col-md-4 sub-card">
-          <div class="">
-            <img src={dayPlanner} className="projects" alt="day planner book" />
-            <div class="card-body">
-              <p class="card-text">Day Planner</p>
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 p-5 border border-warning card col-6 col-md-4 sub-card">
-          <div class="">
-            <img src={readme} className="projects" alt="syntax markdown" />
-            <div class="card-body">
-              <p class="card-text">ReadME Generator</p>
-            </div>
-          </div>
-        </div>
-        <div className="mt-5 p-5 border border-warning card col-6 col-md-4 sub-card">
-          <div class="">
-            <img
-              src={passwordGenerator}
-              className="projects"
-              alt="login screen"
+        </figure>
+        <figure className=" mt-5 p-5 card col-6 col-md-4" style={{ width: "26rem" }}>
+          <div className="snip1273">
+          <img
+            src={enlighten}
+            alt="Young adults having fun"
+            className="projects"
             />
-            <div class="card-body">
-              <p class="card-text">Password Generator</p>
-            </div>
+            <figcaption>
+              <h3 className="text-center">enlighten me</h3>
+              <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+              </ul>
+              <div>
+              <a
+                href="https://fierce-spire-72565.herokuapp.com/"
+                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn" target="blank"
+              >
+                {"\u2192"}
+              </a>
+              </div>
+            </figcaption>
           </div>
-        </div>
+        </figure>
+        <figure className=" mt-5 p-5 card col-6 col-md-4">
+          <div className="snip1273">
+          <img
+            src={eCommerce}
+            alt="keyboard enter button with shopping cart on it"
+            className="projects"
+            />
+            <figcaption>
+              <h3 className="text-center">eCommerce</h3>
+              <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+              </ul>
+              <div>
+              <a
+                href="https://github.com/JosieMald/e-commerce-back-end"
+                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn" target="blank"
+              >
+                {"\u2192"}
+              </a>
+              </div>
+            </figcaption>
+          </div>
+        </figure>
+        <figure className=" mt-5 p-5 card col-6 col-md-4">
+          <div className="snip1273">
+          <img
+            src={dayPlanner}
+            alt="planning notebook"
+            className="projects"
+            />
+            <figcaption>
+              <h3 className="text-center">day planner</h3>
+              <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+              </ul>
+              <div>
+              <a
+                href="https://josiemald.github.io/work-day-scheduler/"
+                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn" target="blank"
+              >
+                {"\u2192"}
+              </a>
+              </div>
+            </figcaption>
+          </div>
+        </figure>
+        <figure className=" mt-5 p-5 card col-6 col-md-4">
+          <div className="snip1273">
+          <img
+            src={readme}
+            alt="laptop"
+            className="projects"
+            />
+            <figcaption>
+              <h3 className="text-center">Readme</h3>
+              <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+              </ul>
+              <div>
+              <a
+                href="https://josiemald.github.io/library-congress-SE/"
+                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn" target="blank"
+              >
+                {"\u2192"}
+              </a>
+              </div>
+            </figcaption>
+          </div>
+        </figure>
+        <figure className=" mt-5 p-5 card col-6 col-md-4">
+          <div className="snip1273">
+          <img
+            src={projectTracker}
+            alt="login screen"
+            className="projects"
+            />
+            <figcaption>
+              <h3 className="text-center">project tracker</h3>
+              <ul>
+                <li>hello</li>
+                <li>hello</li>
+                <li>hello</li>
+              </ul>
+              <div>
+              <a
+                href="https://josiemald.github.io/project-tracker/"
+                className="col-2 border border-warning p-2 bg-warning rounded-pill text-black-50 link-btn" target="blank"
+              >
+                {"\u2192"}
+              </a>
+              </div>
+            </figcaption>
+          </div>
+        </figure>
       </div>
     </div>
   );
